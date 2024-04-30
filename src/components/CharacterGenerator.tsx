08ba43c11976cc8f8f7a73d809632513968910ed
John Chastain <jchastain@gogeoh.com>
2024-04-30 17:32:15 -0400

    add taggedItems

--- src/components/CharacterGenerator.tsx
+++ src/components/CharacterGenerator.tsx
@@ -48,41 +48,41 @@ const gender = [
 ]
 
 const title = [
-  {name: 'Master', tags: [ Gender.Neutral, 'Artisan' ]},
-  {name: 'Apprentice', tags: [ Gender.Neutral, 'Artisan' ]},
-  {name: 'Chamberlain', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Sheriff', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Alderman', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Lord Mayor', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Burgomaster', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Chancellor', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Governor', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Minister', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Magistrate', tags: [ Gender.Neutral, 'Royal' ]},
-  {name: 'Bishop', tags: [ Gender.Neutral, 'Ecclesiastical' ]},
-  {name: 'Curate', tags: [ Gender.Neutral, 'Ecclesiastical' ]},
-  {name: 'Vicar', tags: [ Gender.Male, 'Ecclesiastical' ]},
-  {name: 'Brother', tags: [ Gender.Male, 'Ecclesiastical' ]},
-  {name: 'Abbot', tags: [ Gender.Male, 'Ecclesiastical' ]},
-  {name: 'Prior', tags: [ Gender.Male, 'Ecclesiastical' ]},
-  {name: 'Friar', tags: [ Gender.Male, 'Ecclesiastical' ]},
-  {name: 'Lord', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'King', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Prince', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Duke', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Marquis', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Earl/Count', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Baron', tags: [ Gender.Male, 'Royal' ]},
-  {name: 'Lady', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Queen', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Princess', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Duchess', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Marchioness', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Countess', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Baroness', tags: [ Gender.Female, 'Royal' ]},
-  {name: 'Sister', tags: [ Gender.Female, 'Ecclesiastical' ]},
-  {name: 'Abbess', tags: [ Gender.Female, 'Ecclesiastical' ]},
-  {name: 'Prioress', tags: [ Gender.Female, 'Ecclesiastical' ]},
+  {name: 'Master', tags: [ Gender.Neutral, 'Artisan', 'All' ]},
+  {name: 'Apprentice', tags: [ Gender.Neutral, 'Artisan', 'All' ]},
+  {name: 'Chamberlain', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Sheriff', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Alderman', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Lord Mayor', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Burgomaster', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Chancellor', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Governor', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Minister', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Magistrate', tags: [ Gender.Neutral, 'Royal', 'All' ]},
+  {name: 'Bishop', tags: [ Gender.Neutral, 'Ecclesiastical', 'All' ]},
+  {name: 'Curate', tags: [ Gender.Neutral, 'Ecclesiastical', 'All' ]},
+  {name: 'Vicar', tags: [ Gender.Male, 'Ecclesiastical', 'All' ]},
+  {name: 'Brother', tags: [ Gender.Male, 'Ecclesiastical', 'All' ]},
+  {name: 'Abbot', tags: [ Gender.Male, 'Ecclesiastical', 'All' ]},
+  {name: 'Prior', tags: [ Gender.Male, 'Ecclesiastical', 'All' ]},
+  {name: 'Friar', tags: [ Gender.Male, 'Ecclesiastical', 'All' ]},
+  {name: 'Lord', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'King', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Prince', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Duke', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Marquis', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Earl/Count', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Baron', tags: [ Gender.Male, 'Royal', 'All' ]},
+  {name: 'Lady', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Queen', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Princess', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Duchess', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Marchioness', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Countess', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Baroness', tags: [ Gender.Female, 'Royal', 'All' ]},
+  {name: 'Sister', tags: [ Gender.Female, 'Ecclesiastical', 'All' ]},
+  {name: 'Abbess', tags: [ Gender.Female, 'Ecclesiastical', 'All' ]},
+  {name: 'Prioress', tags: [ Gender.Female, 'Ecclesiastical', 'All' ]},
 ]
 
 const bTitle = [
@@ -240,7 +240,11 @@ const getCharacter = () => {
 
     } else if ('tags' in functionOrArray) {
       const gender = baseCharacter.gender || Gender.Neutral
-      const array = functionOrArray.filter(({ tags }) => tags ? containsAll([gender], tags) : false)
+      const array = functionOrArray.filter((i) => {
+        if (typeof i !== 'string') {
+          return containsAll([gender], i.tags)
+        }
+      })
       result = getItem(array)
 
     } else if (Array.isArray(functionOrArray)) {
