317afc5e99d28e8c5ca41af554bc161ee32ae58f
John Chastain <jchastain@gogeoh.com>
2024-04-30 17:16:23 -0400

    more configuration of character generator

--- src/components/CharacterGenerator.tsx
+++ src/components/CharacterGenerator.tsx
@@ -2,6 +2,13 @@ import { useState } from 'react';
 import { getItem, getLongName } from '../functions/functions';
 import { Character, Gender } from '../model/Character';
 
+
+
+
+const containsAll = (required: string[], target: string[]) => (
+  required.every((s: string) => target.includes(s))
+);
+
 const age = [
   'Child',
   'Adolescent',
@@ -40,6 +47,44 @@ const gender = [
   Gender.Neutral
 ]
 
+const title = [
+  {name: 'Master', tags: [ Gender.Neutral, 'Artisan' ]},
+  {name: 'Apprentice', tags: [ Gender.Neutral, 'Artisan' ]},
+  {name: 'Chamberlain', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Sheriff', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Alderman', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Lord Mayor', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Burgomaster', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Chancellor', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Governor', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Minister', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Magistrate', tags: [ Gender.Neutral, 'Royal' ]},
+  {name: 'Bishop', tags: [ Gender.Neutral, 'Ecclesiastical' ]},
+  {name: 'Curate', tags: [ Gender.Neutral, 'Ecclesiastical' ]},
+  {name: 'Vicar', tags: [ Gender.Male, 'Ecclesiastical' ]},
+  {name: 'Brother', tags: [ Gender.Male, 'Ecclesiastical' ]},
+  {name: 'Abbot', tags: [ Gender.Male, 'Ecclesiastical' ]},
+  {name: 'Prior', tags: [ Gender.Male, 'Ecclesiastical' ]},
+  {name: 'Friar', tags: [ Gender.Male, 'Ecclesiastical' ]},
+  {name: 'Lord', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'King', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Prince', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Duke', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Marquis', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Earl/Count', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Baron', tags: [ Gender.Male, 'Royal' ]},
+  {name: 'Lady', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Queen', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Princess', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Duchess', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Marchioness', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Countess', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Baroness', tags: [ Gender.Female, 'Royal' ]},
+  {name: 'Sister', tags: [ Gender.Female, 'Ecclesiastical' ]},
+  {name: 'Abbess', tags: [ Gender.Female, 'Ecclesiastical' ]},
+  {name: 'Prioress', tags: [ Gender.Female, 'Ecclesiastical' ]},
+]
+
 const bTitle = [
   'Master',
   'Apprentice',
@@ -52,11 +97,22 @@ const bTitle = [
   'Governor',
   'Minister',
   'Magistrate',
-  'Bishop',
-  'Curate'
 ]
 
-const mTitle = [
+const bReligiousTitle = [
+  'Bishop',
+  'Curate',
+  'Vicar'
+]
+
+const mReligiousTitle = [
+  'Brother',
+  'Abbot',
+  'Prior',
+  'Friar'
+]
+
+const mRoyalTitle = [
   'Lord',
   'King',
   'Prince',
@@ -64,11 +120,9 @@ const mTitle = [
   'Marquis',
   'Earl/Count',
   'Baron',
-  'Brother',
-  'Abbot',
 ]
 
-const fTitle = [
+const fRoyalTitle = [
   'Lady',
   'Queen',
   'Princess',
@@ -76,8 +130,12 @@ const fTitle = [
   'Marchioness',
   'Countess',
   'Baroness',
+]
+
+const fReligiousTitle = [
   'Sister',
-  'Abbess'
+  'Abbess',
+  'Prioress'
 ]
 
 export type Title = {
@@ -86,11 +144,11 @@ export type Title = {
   neutral: string[]
 }
 
-const title: Title = {
-  male: [ ...bTitle, ...mTitle ],
-  female: [ ...bTitle, ...fTitle ],
-  neutral: bTitle
-}
+// const title: Title = {
+//   male: [ ...bTitle, ...mRoyalTitle ],
+//   female: [ ...bTitle, ...fRoyalTitle ],
+//   neutral: bTitle
+// }
 
 
 
@@ -140,16 +198,22 @@ const profession = [
   'Ecclesiastical'
 ]
 
+export type TaggedItem = { 
+  name: string, 
+  tags: String[] 
+}
+
 export type Config = {
   name: Function
   age: String[]
   socialStatus: String[]
   wealth: String[]
   gender: String[]
-  title: {
-    male: String[], 
-    female: String[]
-  }
+  // title: {
+  //   male: String[], 
+  //   female: String[]
+  // }
+  title: TaggedItem[]
   profession: String[]
   demeanor: String[]
 }
@@ -174,15 +238,15 @@ const getCharacter = () => {
     if (functionOrArray instanceof Function) {
       result = functionOrArray()
 
-    }  else if (Array.isArray(functionOrArray)) {
-      result = getItem(functionOrArray)
-
-    } else {
-      const gender = baseCharacter.gender
-      const array = functionOrArray[gender as keyof typeof functionOrArray]
+    } else if ('tags' in functionOrArray) {
+      const gender = baseCharacter.gender || Gender.Neutral
+      const array = functionOrArray.filter(({ tags }) => tags ? containsAll([gender], tags) : false)
       result = getItem(array)
 
-    }
+    } else if (Array.isArray(functionOrArray)) {
+      result = getItem(functionOrArray)
+
+    } 
     baseCharacter[prop as keyof Character] = result
   }
   return baseCharacter
