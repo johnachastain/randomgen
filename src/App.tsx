febe57e7b9a5c9b3c3139f53b7e79dffa212ab08
John Chastain <jchastain@gogeoh.com>
2024-05-08 16:58:06 -0400

    txt edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { RecoilRoot } from "recoil";
 import './App.css'
 import { Route, Switch } from "wouter";
 import { Base } from "./pages/base/Base";
@@ -6,17 +5,13 @@ import { Geomorph } from "./pages/geomorph/Geomorph";
 import { Character } from "./pages/character/Character";
 
 function App() {
-  // test();
-
   return (
-      <RecoilRoot>
-        <Switch>
-          <Route path="/" component={Base} />
-          <Route path="/geomorph" component={Geomorph} />
-          <Route path="/character" component={Character} />
-          <Route>404: No such page!</Route>
-        </Switch>
-      </RecoilRoot>
+    <Switch>
+      <Route path="/" component={Base} />
+      <Route path="/geomorph" component={Geomorph} />
+      <Route path="/character" component={Character} />
+      <Route>404: No such page!</Route>
+    </Switch>
   )
 }
 
